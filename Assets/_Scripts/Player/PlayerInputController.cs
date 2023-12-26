5f814ffa7fe4efbdfce7a9ba829ad2c576b293a9
Stevejobjong <58843907+Stevejobjong@users.noreply.github.com>
2023-12-26 13:56:53 +0900

    [💫 Feature] 레이저 추가

--- Assets/_Scripts/Player/PlayerInputController.cs
+++ Assets/_Scripts/Player/PlayerInputController.cs
@@ -7,11 +7,14 @@ public class PlayerInputController : MonoBehaviour
 {
     #region Fields
     [SerializeField] private Transform _playerSpine;
+    [SerializeField] private Transform _muzzle;
     private Transform _playerTransform;
     private float _rotX;
     private float _rotY;
     private float _sensitivity = 10f;
     private float _clampAngle = 70f;
+
+    public LayerMask BulletLayer;
     #endregion
     private void Awake()
     {
@@ -29,5 +32,24 @@ public class PlayerInputController : MonoBehaviour
 
         _playerTransform.rotation = Quaternion.Euler(0, _rotY, 0);
         _playerSpine.rotation = Quaternion.Euler(_rotX, _rotY, 0);
+    }
+    void OnDrawGizmos()
+    {
+
+        float maxDistance = 100;
+        RaycastHit hit;
+        // Physics.Raycast (레이저를 발사할 위치, 발사 방향, 충돌 결과, 최대 거리)
+        bool isHit = Physics.Raycast(_muzzle.position, _muzzle.right, out hit, maxDistance, BulletLayer);
+
+        Gizmos.color = Color.red;
+
+        if (isHit)
+        {
+            Gizmos.DrawRay(_muzzle.position, _muzzle.right * hit.distance);
+        }
+        else
+        {
+            Gizmos.DrawRay(_muzzle.position, _muzzle.right * maxDistance);
+        }
     }
 }
